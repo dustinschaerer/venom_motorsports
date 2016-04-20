2811b2ec2d4d97c407f8755beddf0c9c90f2e0a5
Dustin Schaerer <dustinschaerer@gmail.com>
2016-04-20 12:16:23 -0700

    Style up posts show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,11 @@
 //= require trix
 //= require custom
 //= require_tree .
+
+//= require blueimp-gallery
+//= require blueimp-gallery-fullscreen
+//= require blueimp-gallery-indicator
+//= require blueimp-gallery-video
+//= require blueimp-gallery-youtube
+//= require jquery.blueimp-gallery
+//= require bootstrap-image-gallery
