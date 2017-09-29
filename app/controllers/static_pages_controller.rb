38e061364d1682b2af9074bab26ee173dd9b94fb
Dustin Schaerer <dustinschaerer@gmail.com>
2017-09-29 12:46:42 -0700

    Delete vars from template

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   before_action :authenticate_admin_user!, only: [:manage]
 
   def home
-    @posts = Post.order(created_at: :desc).limit(10)
-    @setting = Setting.first
-    @services = Service.all.order(:sort)
+    # @posts = Post.order(created_at: :desc).limit(10)
+    # @setting = Setting.first
+    # @services = Service.all.order(:sort)
   end
 
   def manage
