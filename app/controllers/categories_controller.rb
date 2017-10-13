efa507d0e824fe7623d64d590c70582c1d3a05bb
Dustin Schaerer <dustinschaerer@gmail.com>
2017-10-13 16:13:16 -0700

    More revisions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,7 +21,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     respond_to do |format|
       if @category.save(category_params)
-        format.html { redirect_to "/#{@category.slug}", notice: 'Post was successfully updated.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully updated.' }
       else
         format.html { render :edit }
       end
@@ -31,7 +31,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to "/#{@category.slug}", notice: 'Post was successfully updated.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully updated.' }
       else
         format.html { render :edit }
       end
